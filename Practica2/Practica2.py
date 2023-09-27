42e026f8aa332567a895629d7103eb5556fe69fc
David2003006 <davidalejandro2003@yahoo.com>
2023-09-26 18:24:09 -0700

    Practica2-correcion

--- Practica2/Practica2.py
+++ Practica2/Practica2.py
@@ -1,19 +1,25 @@
 import pandas as pd
 
 
-data= pd.read_csv("top_10000_1960-now.csv", encoding='latin1')
+ruta ="top_10000_1960-now.csv"
+pd= pd.read_csv(ruta)
+
 
 columns_to_delete = [
     'Track URI', 'Artist URI(s)', 'Album URI', 'Album Artist URI(s)', 
     'Album Image URL', 'Track Preview URL', 'Added By', 'Added At', 
     'Album Genres', 'Copyrights', 'Mode', 'Loudness', 'Energy', 
     'Danceability', 'Speechiness', 'Acousticness', 'Instrumentalness', 
-    'Liveness', 'Time Signature'
+    'Liveness', 'Time Signature', 'ISRC', 'Album Artist Name(s)'
 ]
 
+generos = ["indie", "rock", "metal", "britpop", "australian", "punk", "alternativo", "pop", "rap", "r&b", "post-grunge", "dance pop", "disco", "folk"]
+
+
+    
 #Aqui borro las columnas inecesarias de mi dataset 
-def delete_colum(data, columns_to_delete ):
-    data.drop(columns=columns_to_delete, inplace=True)
+def delete_colum(pd, columns_to_delete ):
+    pd.drop(columns=columns_to_delete, inplace=True)
     
 
 #Aqui en la columna del 'Tempo' checo que numero es para regresar Lento, Moderado etc... 
@@ -44,13 +50,32 @@ def define_Emocion(valor):
         return 'Feliz'
     else:
         return 'Neutro'
-       
 
-delete_colum(data, columns_to_delete)    
-data['Tempo']= data['Tempo'].apply(define_tempo)
-data = data.rename(columns={'Valence': 'Emocionalidad'})
-data= data.rename(columns={'Label':'Disquera'})
-data['Emocionalidad']=data['Emocionalidad'].apply(define_Emocion)
-data.to_csv('Canciones_1960-2023.csv', index= False)
+def convert_ms_to_min_sec(ms):
+    seconds = ms / 1000
+    minutes = seconds // 60
+    seconds = seconds % 60
+    return f"{int(minutes)} minutos {int(seconds)} segundos"
+
+def asignar_genero(row):
+    for genero in generos:
+        row[genero] = 0  # Establecer valor inicial en 0 para cada género
+        artist_genres = str(row['Artist Genres']).lower()
+    for genero in generos:
+        if genero in artist_genres:
+            row[genero] = 1
+    return row
+
+pd = pd.apply(asignar_genero, axis=1)
+pd.drop(columns=['Artist Genres'], inplace=True)
+delete_colum(pd, columns_to_delete)    
+pd['Tempo']= pd['Tempo'].apply(define_tempo)
+pd = pd.rename(columns={'Valence': 'Emocionalidad'})
+pd['Track Duration (ms)'] = pd['Track Duration (ms)'].apply(convert_ms_to_min_sec)
+pd= pd.rename(columns={'Label':'Disquera'})
+pd['Emocionalidad']=pd['Emocionalidad'].apply(define_Emocion)
+pd.to_csv('canciones_1960-2023.csv', index= False)
+
+
 
     
